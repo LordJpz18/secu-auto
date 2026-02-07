64918d2e638a4b2351b3142eec7d975c95eec8cc
LordJP18 <164006281+LordJpz18@users.noreply.github.com>
2026-02-07 11:11:44 +0100

    tp

---
+++ exercices.py
@@ -0,0 +1,91 @@
+"""
+TP Python - Exercices de base.
+Completer chaque fonction en suivant l enonce du site.
+"""
+
+
+def addition_soustraction(a, b):
+    """
+    Retourner un tuple (somme, difference) a partir de a et b.
+    """
+    # TODO: calculer la somme et la difference
+    pass
+
+
+def multiplication_division(a, b):
+    """
+    Retourner un tuple (produit, quotient) a partir de a et b.
+    Le quotient doit etre un float.
+    """
+    # TODO: calculer le produit et le quotient
+    pass
+
+
+def aire_rectangle(longueur, largeur):
+    """
+    Retourner l aire d un rectangle.
+    """
+    # TODO: calculer l aire
+    pass
+
+
+def moyenne_trois(a, b, c):
+    """
+    Retourner la moyenne de trois nombres.
+    """
+    # TODO: calculer la moyenne
+    pass
+
+
+def nom_complet(prenom, nom):
+    """
+    Retourner une chaine "Prenom Nom".
+    """
+    # TODO: concatenation avec un espace
+    pass
+
+
+def compter_voyelles(texte):
+    """
+    Compter le nombre de voyelles dans texte.
+    Voyelles: a, e, i, o, u, y (minuscules et majuscules).
+    """
+    # TODO: boucle et compteur
+    pass
+
+
+def inverser_chaine(texte):
+    """
+    Retourner une nouvelle chaine avec les caracteres de texte en ordre inverse.
+    """
+    # TODO: construire la chaine inverse
+    pass
+
+
+def table_multiplication(n):
+    """
+    Afficher la table de multiplication de n, de 1 a 10.
+    Exemple d affichage: "3 x 4 = 12"
+    """
+    # TODO: boucle for et print
+    pass
+
+
+def somme_de_1_a_n(n):
+    """
+    Calculer et afficher la somme des entiers de 1 a n inclus.
+    """
+    # TODO: boucle et somme
+    pass
+
+
+def mot_magique(n):
+    """
+    Pour i de 1 a n:
+    - afficher "Boum" si i est multiple de 3
+    - afficher "Choc" si i est multiple de 5
+    - afficher "BoumChoc" si i est multiple de 3 et 5
+    - sinon afficher i
+    """
+    # TODO: boucle, operations et affichage
+    pass
